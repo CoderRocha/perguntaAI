7bc922f1e1ea0318c22a1bc097cde6a289233331
Guilherme <guilhermerochas0420@gmail.com>
2026-03-25 19:31:14 -0300

    init repository

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ChatBotStart from './Components/ChatBotStart'
+
+export default function App() {
+  return (
+    <div>
+      <ChatBotStart />
+    </div>
+  )
+}
---
+++ src/Components/ChatBotStart.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ChatBotStart() {
+  return (
+    <div>Pergunta AI</div>
+  )
+}
